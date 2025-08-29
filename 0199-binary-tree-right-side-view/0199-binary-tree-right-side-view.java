5f8d8a22c1e906aac3bc262559611c15e521f379
Homie_Tomie <94494352+HomieTomie@users.noreply.github.com>
2025-08-29 10:53:00 +0200

    0199-BINARY-TREE-RIGHT-SIDE-VIEW Stats: Time: 0 ms (100%), Space: 42.2 MB (43.6%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,25 +14,41 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
+    // public List<Integer> rightSideView(TreeNode root) {
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     List<Integer> result = new ArrayList<>();
+    //     if(root==null) return result;
+    //     q.add(root);
+
+    //     while(!q.isEmpty()){
+    //         int levelLength = q.size();
+    //         for(int i = 0; i<levelLength-1; i++){
+    //             TreeNode node = q.poll();
+    //             if(node.left!=null) q.add(node.left);
+    //             if(node.right!=null) q.add(node.right);
+    //         }
+    //         TreeNode n = q.poll();
+    //         result.add(n.val);
+    //         if(n.left!=null) q.add(n.left);
+    //         if(n.right!=null) q.add(n.right);
+    //     }
+
+    //     return result;
+    // }
+
+    public List<Integer> rightSideView(TreeNode root){
         List<Integer> result = new ArrayList<>();
-        if(root==null) return result;
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int levelLength = q.size();
-            for(int i = 0; i<levelLength-1; i++){
-                TreeNode node = q.poll();
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-            }
-            TreeNode n = q.poll();
-            result.add(n.val);
-            if(n.left!=null) q.add(n.left);
-            if(n.right!=null) q.add(n.right);
-        }
-
+        helperFunction(root, result, 0);
         return result;
+    }
+
+    List<Integer> helperFunction(TreeNode root, List<Integer> list, int level){
+        if(root==null) return list;
+
+        if(level == list.size()) list.add(root.val); level++;
+
+        if(root.right!=null) helperFunction(root.right, list, level);
+        if(root.left!=null) helperFunction(root.left, list, level);
+        return list;
     }
 }
