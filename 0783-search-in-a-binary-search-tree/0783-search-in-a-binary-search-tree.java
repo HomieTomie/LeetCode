9f66b779d15dc66452e9e155e933b32cacca4378
Homie_Tomie <94494352+HomieTomie@users.noreply.github.com>
2025-08-31 19:40:58 +0200

    0783-SEARCH-IN-A-BINARY-SEARCH-TREE Stats: Time: 0 ms (100%), Space: 45.4 MB (55.54%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -14,11 +14,15 @@
  * }
  */
 class Solution {
-    TreeNode node;
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root.val == val) node = root;
-        if (root.left!=null) searchBST(root.left, val);
-        if (root.right!=null) searchBST(root.right, val);
-        return node;
+        if (root == null) return null;
+        
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val){
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
     }
 }
