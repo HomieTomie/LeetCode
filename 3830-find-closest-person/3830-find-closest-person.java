95814930a3a5ec4010c24308b353d41c9e3f4a77
Homie_Tomie <94494352+HomieTomie@users.noreply.github.com>
2025-09-04 11:04:06 +0200

    3830-FIND-CLOSEST-PERSON Stats: Time: 0 ms (100%), Space: 40.9 MB (71.18%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.java
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -4,6 +4,9 @@ class Solution {
         // else if (Math.abs(z-y)<Math.abs(z-x)) return 2;
         // else return 0;
 
-        return (Math.abs(z-x)<Math.abs(z-y)) ? 1 : ((Math.abs(z-y)<Math.abs(z-x))? 2 : 0);
+        // return (Math.abs(z-x)<Math.abs(z-y)) ? 1 : ((Math.abs(z-y)<Math.abs(z-x))? 2 : 0);
+
+        int d = (x-y) * (x + y - 2 * z);
+        return (d!=0 ? 1: 0) << (d > 0 ? 1 : 0);
     }
 }
